3ff1ec491b6b983f11a8c11ffdf541e9ce0cf48b
Jake Tucker <89418308+jTucker583@users.noreply.github.com>
2023-08-01 21:13:29 -0600

    Delete robot_control/build directory

--- robot_control/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jake/COHRINT/teleoperation/robot_control/src
-# Build directory: /home/jake/COHRINT/teleoperation/robot_control/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
